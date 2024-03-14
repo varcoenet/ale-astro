d4cd077e90a67c1a576cc83f4e08e806f05eb323
varcoenet <67181866+varcoenet@users.noreply.github.com>
2024-03-14 04:56:51 -0400

    blog

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,16 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+    categories: z.array(z.string()), // Update to include categories
+  }),
 });
 
 export const collections = { blog };
