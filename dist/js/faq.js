6fcb20eda54e4ee3b66dbada148ec888afebd8b0
wraith1200 <ebrown@aleforge.net>
2024-03-24 12:21:49 -0400

    cleanup

--- dist/js/faq.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function (e) {
-    
-if (jQuery(".toggle .toggle-title").hasClass('active')) {
-    jQuery(".toggle .toggle-title.active").closest('.toggle').find('.toggle-inner').show();
-}
-
-});
